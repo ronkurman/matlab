757780d0f200ea0fec4ae204553d5de37b76fd03
ronkurman <69154621+ronkurman@users.noreply.github.com>
2021-01-05 17:42:46 +0200

    Add files via upload

---
+++ proj_bound.m
@@ -0,0 +1,21 @@
+function xp=proj_bound(y,alpha)
+n=length(y);
+lb=min(y)-2/length(y);
+ub=max(y);
+f=@(lam)sum(max(y-lam,0))-1;
+if alpha<1/n
+    fprintf("Empty Set")
+    return
+end
+lam=bisection(f,lb,ub,1e-10);
+lam
+
+for i=1:n
+    
+     if y(i)-lam<=alpha
+        xp(i)=max(y(i)-lam,0);
+     end
+     if y(i)-lam>alpha
+       xp(i)=alpha;
+    end
+end
